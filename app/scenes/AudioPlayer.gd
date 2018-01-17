8ae0f6dbebc7db8b1c5beb8cbb9a27785b4ef481
Guilherme Lagemann <gdlagemann@inf.ufpel.edu.br>
2018-01-17 00:53:41 -0200

    Sfx teste funcionando

---
+++ app/scenes/AudioPlayer.gd
@@ -0,0 +1,12 @@
+extends AudioStreamPlayer
+
+
+func _ready():
+	pass
+
+
+func play_sfx():
+	self.play(0)
+	pass
+
+
--- app/scenes/BtnSFX.gd
+++ app/scenes/BtnSFX.gd
@@ -2,6 +2,8 @@ extends TextureButton
 
 var sfx = ""
 
+onready var audio_player = get_node("AudioPlayer")
+
 func _ready():
 	pass
 
@@ -11,7 +13,9 @@ func set_label_name(param):
 
 func set_play_sfx(param):
 	sfx = param
+	#audio_player.set("stream", sfx)
 	pass
 
 func set_label_ep(param):
 	get_node("EP").set("text", "#" + param)
+
